5818265938e8693aca2208b5a3db9686e4ed2c98
EfazAhmed <73269488+EfazAhmed@users.noreply.github.com>
2023-08-22 19:50:15 -0400

    changed QueryMappings to allow for lists of objects

--- src/main/java/com/company/gamestore/controller/GraphController.java
+++ src/main/java/com/company/gamestore/controller/GraphController.java
@@ -1,9 +1,14 @@
 package com.company.gamestore.controller;
 
+import com.company.gamestore.model.Game;
+import com.company.gamestore.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class GraphController {
@@ -17,37 +22,37 @@ public class GraphController {
     @QueryMapping
     public Game findGameById(@Argument int id) {
         Optional<Game> returnVal = gameRepository.findById(id);
-        return returnVal.isPresent() ? returnVal.get(): null;
+        return returnVal.isPresent() ? returnVal.get() : null;
     }
 
     @QueryMapping
-    public Game findGameByTitle(@Argument String title) {
-        Optional<Game> returnVal = gameRepository.findByTitle(title);
-        return returnVal.isPresent() ? returnVal.get(): null;
+    public List<Game> findGameByTitle(@Argument String title) {
+        List<Game> returnVal = gameRepository.findByTitle(title);
+        return returnVal.size() > 0 ? returnVal : null;
     }
 
     @QueryMapping
-    public Game findGameByEsrbRating(@Argument String esrbRating) {
-        Optional<Game> returnVal = gameRepository.findByEsrbRating(esrbRating);
-        return returnVal.isPresent() ? returnVal.get(): null;
+    public List<Game> findGameByEsrbRating(@Argument String esrbRating) {
+        List<Game> returnVal = gameRepository.findByEsrbRating(esrbRating);
+        return returnVal.size() > 0 ? returnVal : null;
     }
 
     @QueryMapping
-    public Game findGameByStudio(@Argument String studio) {
-        Optional<Game> returnVal = gameRepository.findByStudio(studio);
-        return returnVal.isPresent() ? returnVal.get(): null;
+    public List<Game> findGameByStudio(@Argument String studio) {
+        List<Game> returnVal = gameRepository.findByStudio(studio);
+        return returnVal.size() > 0 ? returnVal : null;
     }
 
     @QueryMapping
     public Console findConsoleById(@Argument int id) {
         Optional<Console> returnVal = consoleRepository.findById(id);
-        return returnVal.isPresent() ? returnVal.get(): null;
+        return returnVal.isPresent() ? returnVal.get() : null;
     }
 
     @QueryMapping
-    public Console findConsoleByManufacturer(@Argument int manufacturer) {
-        Optional<Console> returnVal = consoleRepository.findByManufacturer(manufacturer);
-        return returnVal.isPresent() ? returnVal.get(): null;
+    public List<Console> findConsoleByManufacturer(@Argument int manufacturer) {
+        List<Console> returnVal = consoleRepository.findByManufacturer(manufacturer);
+        return returnVal.size() > 0 ? returnVal : null;
     }
 
 }
